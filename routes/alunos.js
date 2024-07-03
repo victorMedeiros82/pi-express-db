95bf1c006555338f7765b9a69d2a61df8f254cd5
Victor Medeiros <victor.medeiros82@gmail.com>
2024-07-03 17:17:39 -0300

    Edição do formulario de novo aluno.

--- routes/alunos.js
+++ routes/alunos.js
@@ -10,15 +10,16 @@ router.get('/', function(req, res, next) {
     res.render('all_students',data);
 });
 
+router.get('/new', function(req, res, next) {
+    res.render('form',{title: 'Novo Aluno',buttonText:'Adicionar Aluno'});
+});
+
 router.get('/:matricula', function(req,res,next){
     const{matricula} = req.params;
     const aluno = alunos.content[matricula];
     res.render('details_student',{title:'Detalhe do Aluno',aluno})
 })
 
-router.get('/new', function(req, res, next) {
-    res.render('form',{title: 'Novo Aluno'});
-});
 
 router.get('/edit/:id', function(req, res, next) {
     res.render('form',{title: 'Editar Aluno'});
