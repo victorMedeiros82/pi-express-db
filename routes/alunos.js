17a64fef963e371e1aed26e279898ad461ec0a12
Victor Medeiros <victor.medeiros82@gmail.com>
2024-07-08 17:14:33 -0300

    organizando o arquivo das rotas.

--- routes/alunos.js
+++ routes/alunos.js
@@ -1,7 +1,9 @@
+// IMPORTAÇÃO
 var express = require('express');
 var router = express.Router();
 let alunos = require('../tests/mocks/alunos.json');
 
+// GET
 router.get('/', function(req, res, next) {
     const data = {
         title: 'Alunos',
@@ -9,19 +11,23 @@ router.get('/', function(req, res, next) {
     }
     res.render('all_students',data);
 });
-
 router.get('/new', function(req, res, next) {
     parametro = "create"
     const data= {metodo:"POST",parametro, title: 'Novo Aluno',buttonText:'Adicionar Aluno'}
     res.render('form',data);
 });
-
-router.post('/', function (req, res, next) {
-    const{body,method} = req
-    
-    res.send({body,method});
+router.get('/:matricula', function(req,res,next){
+    const{matricula} = req.params;
+    const aluno = alunos.content[matricula];
+    res.render('details_student',{title:'Detalhes do Aluno',aluno})
+})
+router.get('/edit/:matricula', function(req, res, next) {
+    const{matricula} = req.params;
+    const aluno = alunos.content[matricula];
+    const parametro = matricula;
+    res.render('form',{metodo: "PUT",parametro, title: 'Editar Aluno',buttonText:'Salvar Alterações',aluno});
 });
-
+// POST
 router.post('/create',function(req,res,next){
     let novoAluno = req.body
     let matricula = novoAluno.matricula
@@ -31,22 +37,7 @@ router.post('/create',function(req,res,next){
     }
     res.redirect('/alunos')
 })
-
-
-router.get('/:matricula', function(req,res,next){
-    const{matricula} = req.params;
-    const aluno = alunos.content[matricula];
-    res.render('details_student',{title:'Detalhes do Aluno',aluno})
-})
-
-
-router.get('/edit/:matricula', function(req, res, next) {
-    const{matricula} = req.params;
-    const aluno = alunos.content[matricula];
-    const parametro = matricula;
-    res.render('form',{metodo: "PUT",parametro, title: 'Editar Aluno',buttonText:'Salvar Alterações',aluno});
-});
-
+// PUT 
 router.put('/:matricula', function (req, res, next) {
     // const{body,method} = req
     const {matricula} = req.params;
@@ -57,11 +48,11 @@ router.put('/:matricula', function (req, res, next) {
     res.redirect('/alunos')
     // res.send({body,method,msg:'Alterar o aluno'} );
 });
+// DELETE
 router.delete('/:matricula', function (req, res, next) {
     const {matricula} = req.params;
     delete alunos.content[matricula]
     res.redirect('/alunos')
 });
-
-
+// EXPORTAÇÃO
 module.exports = router;
