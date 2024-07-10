02fffc3b59d88a4253b4237a2d9389463d11f917
Victor Medeiros <victor.medeiros82@gmail.com>
2024-07-10 17:36:56 -0300

    alteração das funções das rotas para conexao de banco de dados.

--- routes/api/api-alunos.js
+++ routes/api/api-alunos.js
@@ -14,30 +14,31 @@ router.get('/', async function(req,res,next){
 })
 router.get('/:matricula', async function(req, res, next) {
     const {matricula} = req.params.matricula;
-    const query = `SELECT * FROM alunos WHERE matricula= ${matricula}`
+    const query = `SELECT * FROM alunos WHERE matricula= $1`
     try {
-        const data = await db.any(query)
+        const data = await db.one(query,matricula)
         res.status(200).json(data);
     } catch (error) {
         res.status(400).json({msg: error.message});
     }
 });
 // POST
-router.post('/',function(req,res,next){
-    const novoAluno = req.body;
-    const matricula = novoAluno.matricula;
-    alunos.content[matricula] = {
-        ...novoAluno,
-        matricula:Number(matricula)
-    }
-    const response = {
-        msg: "Aluno criado com sucesso!",
-        aluno: alunos.content[matricula]
-    }
+router.post('/', async function(req,res,next){
+    const nome = req.body.nome
+    const matricula = req.body.matricula
+    const email = req.body.email
+    const data_nascimento = req.body.data_nascimento
+    console.log(req.body)
+    const query = `
+    INSERT INTO alunos (matricula, nome, email, data_nascimento) VALUES
+    ($1, $2, $3, $4)
+    `
+    const values = [matricula, nome, email, data_nascimento]
     try {
-        res.status(201).json(response)
+        const data = await db.any(query,values)
+        res.status(201).json(data)
     } catch (error) {
-        res.status(400).json({msg: error.msg})
+        res.status(400).json(error)
     }
 })
 // PUT 
@@ -49,6 +50,12 @@ router.put('/:matricula', function (req, res, next) {
         msg: "Aluno editado com sucesso!",
         aluno: alunos.content[matricula]
     }
+    const query = `
+    UPDATE alunos
+    SET
+    nome = $2, email = $3, data_nascimento = $4
+    WHERE matricula= $1   
+    `
     try {
         res.status(200).json(response)
     } catch (error) {
@@ -57,8 +64,9 @@ router.put('/:matricula', function (req, res, next) {
 
 });
 // DELETE
-router.delete('/:matricula', function (req, res, next) {
+router.delete('/:matricula', async function (req, res, next) {
     const {matricula} = req.params.matricula;
+    const query = "DELETE FROM alunos WHERE matricula = $1"
     delete alunos.content[matricula]
     const response = {
         msg: "Aluno excluido!",
